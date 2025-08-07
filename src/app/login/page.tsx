ecf16f4172a98b9eeaba33bf72296a6502b4d2f6
Ricardo <ricardo@scoutbasketball.com>
2025-08-08 00:15:38 +0200

    fix auth 2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,7 +12,7 @@ export default function LoginPage() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${redirectUrl}/auth/callback`,
+        redirectTo: `${location.origin}/auth/callback`,
       },
     })
     setLoading(false)
